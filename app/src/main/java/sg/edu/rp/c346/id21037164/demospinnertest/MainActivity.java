52c6aad11d735e8f7ba93111c6f8b1f907c37832
jmattheww <21037164@myrp.edu.sg>
2022-06-09 23:42:34 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21037164/demospinnertest/MainActivity.java
@@ -0,0 +1,46 @@
+package sg.edu.rp.c346.id21037164.demospinnertest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spn;
+   TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spn = findViewById(R.id.spinner);
+        tv = findViewById(R.id.textview);
+
+        spn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        tv.setText("Spinner Item, Yes Selected");
+                        break;
+
+                    case 1:
+                        tv.setText("Spinner Item, No Selected");
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        {
+        }
+    }
+}
